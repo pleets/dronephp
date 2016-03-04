dba3f8015410657b78a2b2a9fc286b1081ca4712
Darío Rivera <fermius.us@gmail.com>
2016-03-04 14:26:25 -0500

    Fix name of file SQLServerModel

---
+++ module/App/source/model/SQLServerModelExample.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+class SQLServerModelExample extends \Pleets\Sql\AbstractionModel
+{
+    public function consulta()
+    {
+        $sql = "SELECT * FROM SYS.TABLES";
+        $result = $this->getDb()->query($sql);
+        return $this->getDb()->toArray(array('encode_utf8' => true));
+    }
+}
