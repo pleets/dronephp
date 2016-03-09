f171a643ba7867eaf39659848df255b63a6c5e31
Darío Rivera <fermius.us@gmail.com>
2016-03-09 13:20:08 -0500

    Add functions to module/Pleets

--- public/js/pleets.js
+++ public/js/pleets.js
@@ -1,5 +1,92 @@
 $(function(){
 
+    $("[data-role='file-actions']").click(function(event){
+        event.preventDefault();
+
+        var url = $(this).attr('href');
+
+        $.ajax({
+            url: url,
+            data: {},
+            beforeSend: function() {
+                $("#file-functions").openModal();
+            },
+            error: function()
+            {
+                $("#file-functions .modal-content").html('Error!');
+            },
+            success: function(data)
+            {
+                $("#file-functions .modal-content").html(data);
+            }
+        });
+    });
+
+    $("#get-views-from-controller").click(function(event) {
+        event.preventDefault();
+
+        var url = $(this).attr('href');
+        var module = $(this).attr('data-module');
+        var controller = $(this).attr('data-controller');
+
+        $.ajax({
+            url: url,
+            method: 'post',
+            data: { module: module, controller: controller },
+            beforeSend: function()
+            {
+                $("#show-views-from-controller").openModal();
+            },
+            error: function()
+            {
+                $("#show-views-from-controller .modal-content").html('Error!');
+            },
+            success: function(data)
+            {
+                $("#show-views-from-controller .modal-content").html(data);
+            }
+        });
+    });
+
+    $("#save-file-action").submit(function(event)
+    {
+        event.preventDefault();
+
+        var url = $(this).attr('action');
+        var type = $(this).attr('method');
+        var data = $(this).serializeArray();
+
+        $.ajax({
+            url: url,
+            type: type,
+            data: data,
+            dataType: 'json',
+            beforeSend: function()
+            {
+                $("#save-file-action-loader").show(500);
+                $("#save-file-action-response").hide(500)
+                $("#save-file-action-response").empty();
+            },
+            error: function()
+            {
+                $("#save-file-action-response").html("<div class='card-panel red'>Error al guardar los cambios!</div>");
+            },
+            success: function(data)
+            {
+                if (!data.success)
+                    $("#save-file-action-response").html("<div class='card-panel red'>Error al guardar los cambios!</div>");
+            },
+            complete: function()
+            {
+                setTimeout(function(){
+                    $("#save-file-action-response").show();
+                }, 1000)
+
+                $("#save-file-action-loader").hide(500);
+            }
+        });
+    });
+
     $("body").delegate("[data-action='ajax-request']", "click", function(event)
     {
         event.preventDefault();
@@ -9,7 +96,7 @@ $(function(){
         var box = $(this).attr('data-response');
         var data = $(this).attr('data-object');
 
-        var call = eval($(this).attr('data-callback')) || {};        
+        var call = eval($(this).attr('data-callback')) || {};
         call.success = call.success || new Function();
 
         $.ajax({
@@ -29,6 +116,6 @@ $(function(){
                 call.success();
             }
         });
-    });    
+    });
 
 });
