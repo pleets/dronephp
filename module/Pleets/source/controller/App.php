02c519f99aa5c2adf4d1affc6edae5919580c5ff
Dario Antonio Rivera <dario.rivera@enertolima.com>
2016-07-27 10:49:14 -0500

    Add simple version log

--- module/Pleets/source/controller/App.php
+++ module/Pleets/source/controller/App.php
@@ -9,6 +9,69 @@ class App extends AbstractionController
 {
 	public function index()
 	{
+		$data = array();
+		return $data;
+	}
+
+	public function appFullVersion()
+	{
+		$data = array();
+
+		$shell = new Pleets_FileSystem_Shell();
+		$files = $shell->ls($_GET["folder"], true);
+
+		$parsed_files = array();
+		foreach ($files as $file)
+		{
+			if (!in_array($file, array('.','..')) && !is_dir($file))
+				$parsed_files[] = $file;
+		}
+
+		$files = $parsed_files;
+
+        $v_file_name = "cache/xls/app_version" ."_". uniqid() . ".xls";
+        $v_data["file_name"] = $v_file_name;
+
+        $hd = fopen($v_file_name, "a");
+
+        $v_string = "<table>";
+
+        $v_string .= "<thead><tr>";
+
+        $v_string .= "<th style='border: 1px solid rgb(70, 50, 101);'>FILE</th>" .
+                     "<th style='border: 1px solid rgb(70, 50, 101);'>LINES</th>" .
+                     "<th style='border: 1px solid rgb(70, 50, 101);'>CHARACTERS</th>" .
+                     "<th style='border: 1px solid rgb(70, 50, 101);'>SIZE</th>";
+
+        $v_string .= "</tr></thead><body>";
+
+
+        foreach($files as $file)
+        {
+			$desc = $this->describeFile($file);
+
+            $v_string .= "<tr>";
+
+            $v_string .= "<td style='border: 1px solid rgb(6, 162, 236);'>" . $file . "</td>" .
+                         "<td style='border: 1px solid rgb(6, 162, 236);'>" . $desc["lines"] . "</td>" .
+                         "<td style='border: 1px solid rgb(6, 162, 236);'>" . $desc["characters"] . "</td>" .
+                         "<td style='border: 1px solid rgb(6, 162, 236);'>" . $desc["size"] . "</td>";
+
+            $v_string .= "</tr>";
+        }
+
+        $v_string .= "</tbody></table>";
+
+        fwrite($hd, $v_string);
+        fclose($hd);
+
+        $data["file_name"] = $v_file_name;
+
+		return $data;
+	}
+
+	public function modules()
+	{
 		$shell = new Shell();
 		$modules = $shell->ls('module');
 
@@ -20,18 +83,18 @@ class App extends AbstractionController
 		}
 
 		$mods = array();
-		foreach ($parsed_modules as $module) 
+		foreach ($parsed_modules as $module)
 		{
 			$mods[$module] = array();
-			
+
 
 			/* Get controllers */
 			$controllers = $shell->ls('module/'.$module.'/source/controller');
-		
+
 			$parsed_controllers = array();
-			foreach ($controllers as $ctrl) 
+			foreach ($controllers as $ctrl)
 			{
-				if (!in_array($ctrl, array('.','..'))) 
+				if (!in_array($ctrl, array('.','..')))
 				{
 					$desc = $this->describeFile('module/'.$module.'/source/controller/'.$ctrl);
 
@@ -47,9 +110,9 @@ class App extends AbstractionController
 			$models = $shell->ls('module/'.$module.'/source/model');
 
 			$parsed_models = array();
-			foreach ($models as $model) 
+			foreach ($models as $model)
 			{
-				if (!in_array($model, array('.','..'))) 
+				if (!in_array($model, array('.','..')))
 				{
 					$desc = $this->describeFile('module/'.$module.'/source/model/'.$model);
 
@@ -64,14 +127,14 @@ class App extends AbstractionController
 
 			/* Get views */
 			$views_for_controllers = array();
-			foreach ($parsed_controllers as $ctrl) 
+			foreach ($parsed_controllers as $ctrl)
 			{
 				$views = $shell->ls('module/'.$module.'/source/view/'.strstr($ctrl['name'], '.', true) );
 
 				$parsed_views = array();
-				foreach ($views as $view) 
+				foreach ($views as $view)
 				{
-					if (!in_array($view, array('.','..'))) 
+					if (!in_array($view, array('.','..')))
 					{
 						$desc = $this->describeFile('module/'.$module.'/source/view/'.strstr($ctrl['name'], '.', true).'/'.$view);
 
@@ -113,6 +176,7 @@ class App extends AbstractionController
         return array(
             'lines' => $num_lines,
             'characters' => $characters,
+            'size' => filesize($_file)
         );
     }
 
@@ -137,6 +201,6 @@ class App extends AbstractionController
         $return_data["module"] = $_POST["module"];
         $return_data["controller"] = $_POST["controller"];
 
-        return $return_data;    	
+        return $return_data;
     }
 }
