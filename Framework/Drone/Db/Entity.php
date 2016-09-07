ba62e124cc4d96135ae6705959ae4b3e5f23b669
Darío Rivera <fermius.us@gmail.com>
2016-09-07 17:24:58 -0500

    Improve error description showing class name

--- Framework/Drone/Db/Entity.php
+++ Framework/Drone/Db/Entity.php
@@ -47,12 +47,14 @@ abstract class Entity
      */
     public function exchangeArray($data)
     {
+        $class = get_class($this);
+
         foreach ($data as $prop => $value)
         {
             if (property_exists($this, $prop))
                 $this->$prop = $value;
             else
-                throw new Exception("The property '$prop' does not exists in the class!");
+                throw new Exception("The property '$prop' does not exists in the class '$class'");
         }
     }
 }
