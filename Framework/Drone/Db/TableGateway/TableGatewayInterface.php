edf59ba410b50bf8140f05b1ff1994ee9a11f12a
Darío Rivera <fermius.us@gmail.com>
2017-03-15 23:13:19 -0500

    Prepare alpha version, update copyright

--- Framework/Drone/Db/TableGateway/TableGatewayInterface.php
+++ Framework/Drone/Db/TableGateway/TableGatewayInterface.php
@@ -3,7 +3,7 @@
  * DronePHP (http://www.dronephp.com)
  *
  * @link      http://github.com/Pleets/DronePHP
- * @copyright Copyright (c) 2016 DronePHP. (http://www.dronephp.com)
+ * @copyright Copyright (c) 2016-2017 Pleets. (http://www.pleets.org)
  * @license   http://www.dronephp.com/license
  */
 
