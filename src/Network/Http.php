93697e595d3cd1cae8a9d6b9d794e2ee99e877ac
Darío Rivera <fermius.us@gmail.com>
2017-08-25 09:24:15 -0500

    Add HTTP class for header status writing

--- src/Network/Http.php
+++ src/Network/Http.php
@@ -9,61 +9,138 @@
 
 namespace Drone\Network;
 
+use Exception;
+
 class Http
 {
+    /**#@+
+     * Status codes
+     * As per http://php.net/manual/en/function.header.php, See the » HTTP/1.1 specification
+     *
+     * @var integer
+     * @link http://www.faqs.org/rfcs/rfc2616.html
+     */
+    const HTTP_CONTINUE = 100;
+    const HTTP_SWITCHING_PROTOCOLS = 101;
+    const HTTP_OK = 200;
+    const HTTP_CREATED = 201;
+    const HTTP_ACCEPTED = 202;
+    const HTTP_NON_AUTHORITATIVE_INFORMATION = 203;
+    const HTTP_NO_CONTENT = 204;
+    const HTTP_RESET_CONTENT = 205;
+    const HTTP_PARTIAL_CONTENT = 206;
+    const HTTP_MULTIPLE_CHOICES = 300;
+    const HTTP_MOVED_PERMANENTLY = 301;
+    const HTTP_FOUND = 302;
+    const HTTP_SEE_OTHER = 303;
+    const HTTP_NOT_MODIFIED = 304;
+    const HTTP_USE_PROXY = 305;
+    const HTTP_RESERVED = 306;
+    const HTTP_TEMPORARY_REDIRECT = 307;
+    const HTTP_BAD_REQUEST = 400;
+    const HTTP_UNAUTHORIZED = 401;
+    const HTTP_PAYMENT_REQUIRED = 402;
+    const HTTP_FORBIDDEN = 403;
+    const HTTP_NOT_FOUND = 404;
+    const HTTP_METHOD_NOT_ALLOWED = 405;
+    const HTTP_NOT_ACCEPTABLE = 406;
+    const HTTP_PROXY_AUTHENTICATION_REQUIRED = 407;
+    const HTTP_REQUEST_TIMEOUT = 408;
+    const HTTP_CONFLICT = 409;
+    const HTTP_GONE = 410;
+    const HTTP_LENGTH_REQUIRED = 411;
+    const HTTP_PRECONDITION_FAILED = 412;
+    const HTTP_REQUEST_ENTITY_TOO_LARGE = 413;
+    const HTTP_REQUEST_URI_TOO_LONG = 414;
+    const HTTP_UNSUPPORTED_MEDIA_TYPE = 415;
+    const HTTP_REQUESTED_RANGE_NOT_SATISFIABLE = 416;
+    const HTTP_EXPECTATION_FAILED = 417;
+    const HTTP_INTERNAL_SERVER_ERROR = 500;
+    const HTTP_NOT_IMPLEMENTED = 501;
+    const HTTP_BAD_GATEWAY = 502;
+    const HTTP_SERVICE_UNAVAILABLE = 503;
+    const HTTP_GATEWAY_TIMEOUT = 504;
+    const HTTP_VERSION_NOT_SUPPORTED = 505;
+
     /**
-     * Returns http host
+     * Status codes and their respective description
+     *
+     * @var array
+     */
+    protected $httpStatusCodes = array(
+        self::HTTP_CONTINUE                        => 'Continue',
+        self::HTTP_SWITCHING_PROTOCOLS             => 'Switching Protocols',
+        self::HTTP_OK                              => 'OK',
+        self::HTTP_CREATED                         => 'Created',
+        self::HTTP_ACCEPTED                        => 'Accepted',
+        self::HTTP_NON_AUTHORITATIVE_INFORMATION   => 'Non-Authoritative Information',
+        self::HTTP_NO_CONTENT                      => 'No Content',
+        self::HTTP_RESET_CONTENT                   => 'Reset Content',
+        self::HTTP_PARTIAL_CONTENT                 => 'Partial Content',
+        self::HTTP_MULTIPLE_CHOICES                => 'Multiple Choices',
+        self::HTTP_MOVED_PERMANENTLY               => 'Moved Permanently',
+        self::HTTP_FOUND                           => 'Found',
+        self::HTTP_SEE_OTHER                       => 'See Other',
+        self::HTTP_NOT_MODIFIED                    => 'Not Modified',
+        self::HTTP_USE_PROXY                       => 'Use Proxy',
+        self::HTTP_TEMPORARY_REDIRECT              => 'Temporary Redirect',
+        self::HTTP_BAD_REQUEST                     => 'Bad Request',
+        self::HTTP_UNAUTHORIZED                    => 'Unauthorized',
+        self::HTTP_PAYMENT_REQUIRED                => 'Payment Required',
+        self::HTTP_FORBIDDEN                       => 'Forbidden',
+        self::HTTP_NOT_FOUND                       => 'Not Found',
+        self::HTTP_METHOD_NOT_ALLOWED              => 'Method Not Allowed',
+        self::HTTP_NOT_ACCEPTABLE                  => 'Not Acceptable',
+        self::HTTP_PROXY_AUTHENTICATION_REQUIRED   => 'Proxy Authentication Required',
+        self::HTTP_REQUEST_TIMEOUT                 => 'Request Time-out',
+        self::HTTP_CONFLICT                        => 'Conflict',
+        self::HTTP_GONE                            => 'Gone',
+        self::HTTP_LENGTH_REQUIRED                 => 'Length Required',
+        self::HTTP_PRECONDITION_FAILED             => 'Precondition Failed',
+        self::HTTP_REQUEST_ENTITY_TOO_LARGE        => 'Request Entity Too Large',
+        self::HTTP_REQUEST_URI_TOO_LONG            => 'Request-URI Too Large',
+        self::HTTP_UNSUPPORTED_MEDIA_TYPE          => 'Unsupported Media Type',
+        self::HTTP_REQUESTED_RANGE_NOT_SATISFIABLE => 'Requested range not satisfiable',
+        self::HTTP_EXPECTATION_FAILED              => 'Expectation Failed',
+        self::HTTP_INTERNAL_SERVER_ERROR           => 'Internal Server Error',
+        self::HTTP_NOT_IMPLEMENTED                 => 'Not Implemented',
+        self::HTTP_BAD_GATEWAY                     => 'Bad Gateway',
+        self::HTTP_SERVICE_UNAVAILABLE             => 'Service Unavailable',
+        self::HTTP_GATEWAY_TIMEOUT                 => 'Gateway Time-out',
+        self::HTTP_VERSION_NOT_SUPPORTED           => 'HTTP Version not supported'
+    );
+
+    /**
+     * Gets the HTTP Status description from the code
+     *
+     * @param integer $code
      *
      * @return string
      */
-    public static function getHost()
+    public function getStatusText($code)
     {
-        return $_SERVER["HTTP_HOST"];
+        $codes = $this->httpStatusCodes;
+
+        if (!in_array($code, array_keys($codes)))
+            throw new Exception("Status code not supported");
+
+        return $this->httpStatusCodes[$code];
     }
 
     /**
-     * Returns server port
+     * Sets the HTTP Status Header
+     *
+     * @param integer $code
      *
      * @return string
      */
-    public static function getServerPort()
+    public function writeStatus($code)
     {
-        return $_SERVER["SERVER_PORT"];
-    }
+        $description = $this->getStatusText($code);
+        $header = $_SERVER['SERVER_PROTOCOL'] . " $code $description";
 
-    /**
-     * Returns client port
-     *
-     * @return string
-     */
-    public static function getClientPort()
-    {
-        return $_SERVER["REMOTE_PORT"];
-    }
+        header($header);
 
-    /**
-     * Returns server ip
-     *
-     * @return string
-     */
-    public static function getServerIP()
-    {
-        return $_SERVER["SERVER_ADDR"];
-    }
-
-    /**
-     * Returns client ip
-     *
-     * @return string
-     */
-    public static function getClientIP()
-    {
-        if (!empty($_SERVER['HTTP_CLIENT_IP']))
-            return $_SERVER['HTTP_CLIENT_IP'];
-
-        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-            return $_SERVER['HTTP_X_FORWARDED_FOR'];
-
-        return $_SERVER['REMOTE_ADDR'];
+        return $header;
     }
 }
---
+++ src/Network/Server.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * DronePHP (http://www.dronephp.com)
+ *
+ * @link      http://github.com/Pleets/DronePHP
+ * @copyright Copyright (c) 2016-2017 Pleets. (http://www.pleets.org)
+ * @license   http://www.dronephp.com/license
+ */
+
+namespace Drone\Network;
+
+class Server
+{
+    /**
+     * Returns http host
+     *
+     * @return string
+     */
+    public static function getHost()
+    {
+        return $_SERVER["HTTP_HOST"];
+    }
+
+    /**
+     * Returns server port
+     *
+     * @return string
+     */
+    public static function getServerPort()
+    {
+        return $_SERVER["SERVER_PORT"];
+    }
+
+    /**
+     * Returns client port
+     *
+     * @return string
+     */
+    public static function getClientPort()
+    {
+        return $_SERVER["REMOTE_PORT"];
+    }
+
+    /**
+     * Returns server ip
+     *
+     * @return string
+     */
+    public static function getServerIP()
+    {
+        return $_SERVER["SERVER_ADDR"];
+    }
+
+    /**
+     * Returns client ip
+     *
+     * @return string
+     */
+    public static function getClientIP()
+    {
+        if (!empty($_SERVER['HTTP_CLIENT_IP']))
+            return $_SERVER['HTTP_CLIENT_IP'];
+
+        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
