61119ddd82005200df7de239b84ced73f65505e6
Darío Rivera <fermius.us@gmail.com>
2018-05-03 17:41:17 -0500

    Fix module undefined index inside Router

--- src/Mvc/Router.php
+++ src/Mvc/Router.php
@@ -141,6 +141,9 @@ class Router
         $module = (is_null($this->identifiers["module"]) || empty($this->identifiers["module"]))
                     ? $this->routes["defaults"]["module"] : $this->identifiers["module"];
 
+        if (!array_key_exists($module, $this->routes))
+            throw new PageNotFoundException("The key '$module' does not exists in routes!");
+
         $controller = (is_null($this->identifiers["controller"]) || empty($this->identifiers["controller"]))
                     ? $this->routes[$module]["controller"] : $this->identifiers["controller"];
 
@@ -152,7 +155,7 @@ class Router
         if (class_exists($fqn_controller))
             $this->controller = new $fqn_controller($module, $view, $this->basePath);
         else
-            throw new PageNotFoundException("The control class '$fqn_controller' does not exists!", 1);
+            throw new PageNotFoundException("The control class '$fqn_controller' does not exists!");
     }
 
     /**
