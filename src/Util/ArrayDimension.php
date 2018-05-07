55ddd5d913daa88196c0cdd69987fdad758f2572
Darío Rivera <fermius.us@gmail.com>
2018-05-07 15:48:06 -0500

    Add ArrayDimension class

---
+++ src/Util/ArrayDimension.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * DronePHP (http://www.dronephp.com)
+ *
+ * @link      http://github.com/Pleets/DronePHP
+ * @copyright Copyright (c) 2016-2018 Pleets. (http://www.pleets.org)
+ * @license   http://www.dronephp.com/license
+ * @author    Darío Rivera <dario@pleets.org>
+ */
+
+namespace Drone\Util;
+
+/**
+ * ArrayDimension class
+ *
+ * Utils functions to operate arrays
+ */
+class ArrayDimension
+{
+    public static function toUnidimensional(Array $array, $glue)
+    {
+        $new_config = [];
+        $again = false;
+
+        foreach ($array as $param => $configure)
+        {
+            if (is_array($configure))
+            {
+                foreach ($configure as $key => $value)
+                {
+                    $again = true;
+                    $new_config[$param . $glue . $key] = $value;
+                }
+            }
+            else
+                $new_config[$param] = $configure;
+        }
+
+        return (!$again) ? $new_config : self::toUnidimensional($new_config, $glue);
+    }
+}
