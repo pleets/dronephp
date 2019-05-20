659999b1411d7504ccbf348c3d03f858eee9033c
Darío Rivera <fermius.us@gmail.com>
2019-05-19 21:24:27 -0500

    Create simple blade based redering

--- src/Mvc/View.php
+++ src/Mvc/View.php
@@ -20,6 +20,13 @@ class View
     use \Drone\Util\ParamTrait;
 
     /**
+     * Regex for identifiers
+     *
+     * @var string
+     */
+    const IDENTIFIER_REGEX = '[_a-zA-Z][_a-zA-Z0-9]*[.]?[_a-zA-Z0-9]+';
+
+    /**
      * View name
      *
      * @var string
@@ -32,6 +39,13 @@ class View
      * @var string
      */
     protected $path;
+
+    /**
+     * The path rendered views are located
+     *
+     * @var string
+     */
+    protected $cache;
 
     /**
      * Returns the view name
@@ -51,6 +65,16 @@ class View
     public function getPath()
     {
         return $this->path;
+    }
+
+    /**
+     * Returns the cache path
+     *
+     * @return string
+     */
+    public function getCache()
+    {
+        return $this->cache;
     }
 
     /**
@@ -78,6 +102,18 @@ class View
     }
 
     /**
+     * Sets the cache path
+     *
+     * @param string
+     *
+     * @return string
+     */
+    public function setCache($cache)
+    {
+        $this->cache = $cache;
+    }
+
+    /**
      * Constructor
      *
      * @param string $name
@@ -90,14 +126,17 @@ class View
     }
 
     /**
-     * Get the contents of the view
+     * Gets the contents of view
      *
      * @throws Exception\ViewNotFoundException
      *
-     * @return  null
+     * @return  string
      */
-    public function getContents()
+    public function getContents($file = null)
     {
+        $_view = $this->path . DIRECTORY_SEPARATOR .
+            str_replace('.', DIRECTORY_SEPARATOR, !is_null($file) ? $file : $this->name) . '.phtml';
+
         $contents = file_get_contents($_view);
 
         if ($contents === false) {
@@ -118,18 +157,67 @@ class View
             extract($this->getParams(), EXTR_SKIP);
         }
 
-        include $this->getFile();
+        $viewFile = $this->getFile();
+        $view = file_get_contents($viewFile);
+
+        if (preg_match("/@extends\('(" .self::IDENTIFIER_REGEX. ")'\)/", $view, $matches) === 1) {
+            $extendsStm = array_shift($matches);
+            $viewIdentifier = array_shift($matches);
+
+            $view = str_replace($extendsStm, file_get_contents($this->getFile($viewIdentifier)), $view);
+
+            while (preg_match("/@section\('(" .self::IDENTIFIER_REGEX. ")'\)/", $view, $sectionOpening) === 1) {
+                $sectionStm = array_shift($sectionOpening);
+                $sectionIdentifier = array_shift($sectionOpening);
+                $sectionFirstIndex = strpos($view, $sectionStm);
+
+                if (preg_match("/@endsection\('(" . $sectionIdentifier . ")'\)/", $view, $sectionEnding) !== 1) {
+                    // no ending statment
+                    $view = str_replace($sectionStm, '', $view);
+                    continue;
+                }
+
+                $endsectionStm        = array_shift($sectionEnding);
+                $endsectionIdentifier = array_shift($sectionEnding);
+                $endsectionFirstIndex = strpos($view, $endsectionStm);
+
+                $sectionContent = substr(
+                    $view,
+                    $sectionFirstIndex,
+                    $endsectionFirstIndex - $sectionFirstIndex + strlen($endsectionStm)
+                );
+
+                $sectionContentTrimmed = str_replace($sectionStm, '', $sectionContent);
+                $sectionContentTrimmed = str_replace($endsectionStm, '', $sectionContentTrimmed);
+
+                $view = str_replace($sectionContent, '', $view);
+                $view = str_replace("@yield('$sectionIdentifier')", $sectionContentTrimmed, $view);
+            }
+        }
+
+        // replace no match yields
+        while (preg_match("/@yield\('(" .self::IDENTIFIER_REGEX. ")'\)/", $view, $matches) === 1) {
+            $view = preg_replace("/@yield\('(" .self::IDENTIFIER_REGEX. ")'\)/", '', $view);
+        }
+
+        $uid = uniqid() . time();
+
+        file_put_contents($this->cache . DIRECTORY_SEPARATOR . $uid, $view);
+        include $this->cache . DIRECTORY_SEPARATOR . $uid;
+        unlink($this->cache . DIRECTORY_SEPARATOR . $uid);
     }
 
     /**
      * Gets the file path
      *
+     * @param string $file
+     *
      * @return  string
      */
-    private function getFile()
+    private function getFile($file = null)
     {
         $_view = $this->path . DIRECTORY_SEPARATOR .
-            str_replace('.', DIRECTORY_SEPARATOR, $this->name) . '.phtml';
+            str_replace('.', DIRECTORY_SEPARATOR, !is_null($file) ? $file : $this->name) . '.phtml';
 
         if (!file_exists($_view)) {
             throw new Exception\ViewNotFoundException("The view '" .$this->name. "' does not exists");
